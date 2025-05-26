54f677d257e53fc49a0c93bfda85f95e051a7acf
Lorenzo-Domingues <lorenzo.domingues45@gmail.com>
2025-05-26 10:51:03 +0100

    Update head.php

--- roleta_asm/head.php
+++ roleta_asm/head.php
@@ -6,4 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/roleta.css">
     <link rel="stylesheet" href="css/connexion.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
