56a3d1d4afbe85963631c54c45a1ec7af207fce4
Lorenzo-Domingues <lorenzo.domingues45@gmail.com>
2025-06-04 14:34:27 +0100

    Delete roleta_asm directory

--- roleta_asm/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=roleta;charset=utf8', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Erreur de connexion à la base : " . $e->getMessage());
-}
-?>
